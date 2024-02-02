d6d6f79252f715b5792db0f032bd74767e8107e5
raul8005 <rimedina@utpl.edu.ec>
2024-02-01 23:47:17 -0500

    Avance

--- Consulta_2Bim/build.sbt
+++ Consulta_2Bim/build.sbt
@@ -1,10 +1,10 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
-
+a
 ThisBuild / scalaVersion := "3.3.1"
 
 lazy val root = (project in file("."))
   .settings(
     name := "Consulta_2Bim",
-    libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.9.0-M2",
-    libraryDependencies += "com.typesafe.akka" %% "akka-stream" % "2.9.0-M2"
+    libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.8.0",
+    libraryDependencies += "com.typesafe.akka" %% "akka-stream" % "2.8.0"
   )
--- Consulta_2Bim/src/main/scala/App.scala
+++ Consulta_2Bim/src/main/scala/App.scala
@@ -1,11 +1,48 @@
-import akka.actor.ActorSystem
-import akka.stream.scaladsl.{Sink, Source}
-import com.github.tototoshi.csv.*
-import java.io.File
+
+import akka.actor.{Actor, ActorSystem, Props}
+
+// Case class que contiene el mensaje del contador
+case class ContadorMensaje(numero: Int)
+
+// Clase Actor contador
+class ActorContador extends Actor {
+
+  var contador: Int = 0
+
+  // Función para manejar los mensajes que recibe el actor, en este caso mensajes de tipo ContadorMensaje
+  def receive: Receive = {
+    case ContadorMensaje(numero) =>
+      contador += numero
+      sender() ! s"Contador actual: $contador"
+  }
+}
+
+// Clase Actor consumidor
+class ActorConsumidor extends Actor {
+  // Función para manejar los mensajes que recibe el actor
+  def receive: Receive = {
+    case mensaje: String =>
+      println(s"Mensaje recibido: $mensaje")
+  }
+}
+
 object App {
-  @main //
+  @main
+  def main =
+    // Declaramos el sistema de actores
+    val system = ActorSystem("SistemaMensajes")
 
-  def calcular() =
+    // Crea los actores contador y consumidor
+    val actorContador = system.actorOf(Props(new ActorContador), "actorContador")
+    val actorConsumidor = system.actorOf(Props(new ActorConsumidor), "actorConsumidor")
 
-    int val =26
+
+    // Simulamos el envío de mensajes desde el contador al consumidor
+    actorContador.!(ContadorMensaje(1))(actorConsumidor)
+
+    // Esperar un tiempo para procesar los mensajes
+    Thread.sleep(1000)
+
+    // Detener el sistema de actores
+    system.terminate()
 }
